4c7162f1f8782d08b05ca91f01c22072c5f06016
Marcos - Omen <virgala@gmail.com>
2022-12-28 13:14:24 -0300

    terminado insertado de imagen en edicion

--- controllers/guardar_edicion.php
+++ controllers/guardar_edicion.php
@@ -8,13 +8,19 @@ if(isset($_POST["editarNombreProyecto"])){
     $id = $_POST["idCard"];
     $nombreProyecto = $_POST["editarNombreProyecto"];
     $descripcionProyecto = $_POST["editarDescripcionProyecto"];
+    // $nombreArchivoImagen = $_POST["editarNombreImagenProyecto"];
     $imagenProyecto = $_FILES['editarImagenProyecto']['name'];
     $imagenTemporal=$_FILES['editarImagenProyecto']['tmp_name'];
     move_uploaded_file($imagenTemporal,"../views/pictures/".$imagenProyecto);
     $urlProyecto = $_POST["editarUrlProyecto"];
 
     $conexion = new Conexion();
-    $sql = "UPDATE `proyectos` SET `nombre`='$nombreProyecto',`descripcion`='$descripcionProyecto',`url`='$urlProyecto' WHERE `proyectos`.`id` = '$id'";
+    if($imagenProyecto){
+        $sql = "UPDATE `proyectos` SET `nombre`='$nombreProyecto',`descripcion`='$descripcionProyecto',`imagen`='$imagenProyecto',`url`='$urlProyecto' WHERE `proyectos`.`id` = '$id'";
+    }else{
+        $sql = "UPDATE `proyectos` SET `nombre`='$nombreProyecto',`descripcion`='$descripcionProyecto',`url`='$urlProyecto' WHERE `proyectos`.`id` = '$id'";
+    }
+    
     $respuesta = $conexion->ejecutar($sql);
     header("Location:../index.php");
     die();
